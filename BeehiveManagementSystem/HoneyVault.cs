b02f681c3ca0b96269b55147302247380ee15884
Clemens Zobel <zobel.clemens@gmail.com>
2023-10-17 23:04:34 -0400

    Add project files.

---
+++ BeehiveManagementSystem/HoneyVault.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeehiveManagementSystem
+{
+    static class HoneyVault
+    {
+        private static string statusReport = "";
+        public static string StatusReport
+        {
+            get
+            {
+                statusReport = $"Vault report: \n{honey: 0.00} units of honey \n{nectar: 0.00} units of nectar \n";
+
+                if (honey < LOW_LEVEL_WARNING)
+                {
+                    statusReport += "LOW HONEY - ADD A HONEY MANUFACTURER \n";
+                }
+
+                if (nectar < LOW_LEVEL_WARNING)
+                {
+                    statusReport += "LOW NECTAR - ADD A NECTAR COLLECTOR \n";
+                }
+
+                return statusReport;
+            }
+        }
+
+        private static float honey = 25f;
+        private static float nectar = 100f;
+
+        private const float NECTAR_CONVERSION_RATIO = 0.19f;
+        private const float LOW_LEVEL_WARNING = 10f;
+
+        public static void CollectNectar(float amount)
+        {
+            if (amount > 0f)
+            {
+                nectar += amount;
+            }
+        }
+
+        public static void ConvertNectarToHoney(float amount)
+        {
+            if (amount > nectar)
+            {
+                amount = nectar;
+            }
+            
+            nectar -= amount;
+            honey += (amount * NECTAR_CONVERSION_RATIO);
+        }
+
+        public static bool ConsumeHoney(float amount)
+        {
+            if (amount <= honey)
+            {
+                honey -= amount;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ BeehiveManagementSystem/Queen.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.ComponentModel;
+
+namespace BeehiveManagementSystem
+{
+    class Queen : Bee, INotifyPropertyChanged
+    {
+        private Bee[] workers = Array.Empty<Bee>();
+        private float unassignedWorkers = 0f;
+        private float eggs = 0f;
+
+        private const float EGGS_PER_SHIFT = 0.45f;
+        private const float HONEY_PER_UNASSIGNED_WORKER = 0.5f;
+
+        public override float CostPerShift
+        {
+            get
+            {
+                return 2.15f;
+            }
+        }
+
+        static private string statusReport = "";
+
+        public string StatusReport
+        {
+            get
+            {
+                statusReport = $"{HoneyVault.StatusReport} \n \n" +
+                    $"Egg count: {eggs: 0.00} \n" +
+                    $"Unassigned workers: {Math.Round(unassignedWorkers, 2, MidpointRounding.ToZero)} \n" +
+                    $"{WorkerStatus("Nectar Collector")}\n" +
+                    $"{WorkerStatus("Honey Manufacturer")}\n" +
+                    $"{WorkerStatus("Egg Care")}\n" +
+                    $"TOTAL WORKERS: {workers.Length}";
+
+                OnPropertyChanged("StatusReport");
+
+                return statusReport;
+            }
+        }
+
+        public Queen() : base("Queen")
+        {
+            unassignedWorkers = 3;
+            AssignBee("Honey Manufacturer");
+            AssignBee("Nectar Collector");
+            AssignBee("Egg Care");
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged; 
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        
+        protected override void DoJob()
+        {
+            eggs += EGGS_PER_SHIFT;
+
+            foreach (Bee worker in workers)
+            {
+                worker.WorkTheNextShift();
+            }
+
+            HoneyVault.ConsumeHoney(HONEY_PER_UNASSIGNED_WORKER * unassignedWorkers);
+        }
+
+        private void AddWorker(Bee worker)
+        {
+            if (unassignedWorkers >= 1)
+            {
+                unassignedWorkers--;
+                Array.Resize(ref workers, workers.Length + 1);
+                workers[^1] = worker;
+            }
+        }
+
+        public void AssignBee(string job)
+        {
+            switch (job)
+            {
+                case "Honey Manufacturer":
+                    AddWorker(new HoneyManufacturer());
+                    break;
+
+                case "Nectar Collector":
+                    AddWorker(new NectarCollector());
+                    break;
+
+                case "Egg Care":
+                    AddWorker(new EggCare(this));
+                    break;
+            }
+        }
+
+        public void CareForEggs(float eggsToConvert)
+        {
+            if (eggs >= eggsToConvert)
+            {
+                eggs -= eggsToConvert;
+                unassignedWorkers += eggsToConvert;
+            }
+        }
+
+        private string WorkerStatus(string job)
+        {
+            int count = 0;
+            foreach (Bee worker in workers)
+            {
+                if (worker.Job == job)
+                {
+                    count++;
+                }
+            }
+
+            string s = "s";
+            if (count == 1)
+            {
+                s = "";
+            }
+
+            return $"{count} {job} bee{s}";
+        }
+    }
+}
